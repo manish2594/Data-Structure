15142452d1bdbdfe19546ee19506af95066acc49
manish2594 <manish2012.2016@gmail.com>
2020-04-24 08:04:23 +0530

    Update Linear-Search.js

--- Linear-Search.js
+++ Linear-Search.js
@@ -1,1 +1,12 @@
 //Linear Search 
+function linearSearch(arr,num){
+  for(let i=0;i<arr.length;i++){
+  if(arr[i]===num){
+    return i;
+  }
+  }
+  return -1
+}
+
+Time complexity --- O(n)
+Space complexity----O(1)
