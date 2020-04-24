99871601d04e847a7be70631cb0f69dca9bf61ef
manish2594 <manish2012.2016@gmail.com>
2020-04-24 08:55:29 +0530

    Update Binary-Search.js

--- Binary-Search.js
+++ Binary-Search.js
@@ -1,1 +1,27 @@
 // Binary Search
+function binarySearch(arr,num){
+    let left= 0;
+    let right = arr.length;
+
+    while(left<=right){
+        let middle = Math.floor((left+right)/2);
+
+        if(arr[middle]>num){
+            right=middle-1;
+        }else if(arr[middle]<num){
+            left=middle+1;
+        }else{
+            return middle;
+        }
+
+    }
+    return -1;
+}
+
+
+binarySearch([1,3,4,5,6],1) 
+//result 0
+
+
+Time complexity ----O(logN)
+Space complexity----O(1)
