06b217b03168ec35492d67e022f499daf3647c08
manish2594 <manish2012.2016@gmail.com>
2020-04-27 10:56:50 +0530

    Update Bubble-Sort.js

--- Bubble-Sort.js
+++ Bubble-Sort.js
@@ -22,3 +22,33 @@ bubble([3,8,1,6])
 const swap=(arr,indx1,indx2)=>{
  [arr[indx1],arr[indx2]]=[arr[indx2],arr[indx1]];
 }
+
+
+
+
+
+
+
+
+//optmised code
+
+
+function bubble(arr){
+  let noSwap;
+    for(let i=arr.length;i>0;i--){
+        noSwap=true;
+        for(let j=0;j<i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    let temp =arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                     noSwap=false
+                    //swap(arr,j,j+1);
+                }
+          if(noSwap){
+              break;
+          }
+        }
+    }
+    return arr;
+}
