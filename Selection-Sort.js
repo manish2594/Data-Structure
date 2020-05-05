9cea541984a15e5f899684169705a6a10b0e05dd
manish2594 <manish2012.2016@gmail.com>
2020-05-05 07:33:56 +0530

    Update Selection-Sort.js

--- Selection-Sort.js
+++ Selection-Sort.js
@@ -1,1 +1,28 @@
 
+function selectionSort(arr){
+
+for(let i=0;i<arr.length;i++){
+ let min = i;
+   for(let j=i+1;j<arr.length;j++){
+       if(arr[min]>arr[j]){
+           min=j;
+       }
+   }
+
+   console.log('swapped',arr,arr[i],arr[min]);
+
+  
+   let temp = arr[i];
+      arr[i]=arr[min];
+       arr[min]=temp;
+      
+}
+
+return arr;
+
+
+}
+
+//swapping = length of aaray but in bublle sort ,on every compare swap is done
+
+selectionSort([3,5,1,4,2]);
